81401befe917a721b5de0064afa28f764dfc878b
CarterHanford <35673856+CarterHanford@users.noreply.github.com>
2020-02-03 01:20:25 -0600

    commit files from homework 2

---
+++ Week 2/Homework/zoo_exercise.py
@@ -0,0 +1,14 @@
+﻿# coding: utf-8
+fcList = ["FieldSightings_Lola", "FieldSightings_Jimmy"]
+print(fclist)
+# ['FieldSightings_Jimmy', 'FieldSightings_Lola']
+print(fcList)
+# ['FieldSightings_Lola', 'FieldSightings_Jimmy']
+buffList = []
+for fc in fcList:
+    buff = arcpy.Buffer_analysis(fc,fc+"_Buffer","1 kilometer")
+    buffList.append(buff)
+for buff in buffList:
+    arcpy.SelectLayerByLocation_management('Redlands_Schools', 'WITHIN',buff,0,'ADD_TO_SELECTION')
+# <Result 'Redlands_Schools'>
+# <Result 'Redlands_Schools'>
